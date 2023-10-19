6a398b74b96d481c652c6b42112374e1a2b930dd
BrodyDubach <117411713+BrodyDubach@users.noreply.github.com>
2023-10-19 00:01:39 +0000

    Done.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        let domain = p + a + n + ".com";
+        console.log(domain);
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
